b18c41d4632ca8ada9694b17ff189128bb9ac706
selov <selove.jara@gmail.com>
2024-11-04 12:56:34 +0400

    Именованные запросы

--- src/main/java/ru/job4j/social/media/api/repository/PostRepository.java
+++ src/main/java/ru/job4j/social/media/api/repository/PostRepository.java
@@ -1,7 +1,19 @@
 package ru.job4j.social.media.api.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.ListCrudRepository;
 import ru.job4j.social.media.api.model.Post;
+import ru.job4j.social.media.api.model.User;
 
-public interface PostRepository extends CrudRepository<Post, Integer> {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PostRepository extends ListCrudRepository<Post, Integer> {
+
+    List<Post> findByUser(User user);
+
+    List<Post> findByCreatedBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    Page<Post> findByOrderByCreatedDesc(Pageable pageable);
 }
